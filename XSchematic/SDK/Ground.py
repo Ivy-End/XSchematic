82a67d70c8906518a14fa20e9fbff2bc4f788dfe
Ivy-End <suda.ivywk@gmail.com>
2023-12-13 18:14:23 +0800

    Fix ground symbol and remove unnecessary code

--- XSchematic/SDK/Ground.py
+++ XSchematic/SDK/Ground.py
@@ -6,7 +6,6 @@ class Ground(Symbol):
     def __init__(self, sizeCoeff = 1.0) -> None:
         super().__init__()
 
-
         self.drawables.append(Line([ Point(0, 0), Point(0, -1.2) ]))
         self.drawables.append(Line([ Point(-0.6, -1.2), Point(0.6, -1.2) ], lineWidth = 2))
         self.drawables.append(Line([ Point(-0.4, -1.4), Point(0.4, -1.4) ], lineWidth = 2))
