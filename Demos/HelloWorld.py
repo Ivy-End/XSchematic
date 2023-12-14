6385f8a4ad8c6d7f9bfcc49b25386ed78d31f648
Ivy-End <suda.ivywk@gmail.com>
2023-12-14 15:13:41 +0800

    Refactor ground objects in HelloWorld.py

--- Demos/HelloWorld.py
+++ Demos/HelloWorld.py
@@ -8,16 +8,16 @@ if __name__ == "__main__":
     engine = Engine.MatPlotLib()
 
     with CellView.Schematic() as sch:
-        sch += (ground := Ground.Ground())
-        sch += (tlground := Ground.TaillessGround().translate(point = ground.northEast() + Utility.Point(0.5, 0)))
-        sch += (refground := Ground.ReferenceGround().translate(point = tlground.northEast() + Utility.Point(0.5, 0)))
-        sch += (sground := Ground.SignalGround().translate(point = refground.northEast() + Utility.Point(0.5, 0)))
-        sch += (tground := Ground.ThickerGround().translate(point = sground.northEast() + Utility.Point(0.5, 0)))
-        sch += (nground := Ground.NoiselessGround().translate(point = tground.northEast() + Utility.Point(0.5, 0)))
-        sch += (pground := Ground.ProtectiveGround().translate(point = nground.northEast() + Utility.Point(0.5, 0)))
-        sch += (cground := Ground.ChassisGround().translate(point = pground.northEast() + Utility.Point(0.5, 0)))
-        sch += (eground := Ground.EuropeanStyleGround().translate(point = cground.northEast() + Utility.Point(1, 0)))
-        sch += (eground2 := Ground.EuropeanStyleGround2().translate(point = eground.northEast() + Utility.Point(1, 0)))
+        sch += (ground := Ground.Ground().translate(point = Utility.Point(0, 0) - Ground.Ground().center()))
+        sch += (tlground := Ground.TaillessGround().translate(point = Utility.Point(1, 0) - Ground.TaillessGround().center()))
+        sch += (refground := Ground.ReferenceGround().translate(point = Utility.Point(2, 0) - Ground.ReferenceGround().center()))
+        sch += (sground := Ground.SignalGround().translate(point = Utility.Point(3, 0) - Ground.SignalGround().center()))
+        sch += (tground := Ground.ThickerGround().translate(point = Utility.Point(4, 0) - Ground.ThickerGround().center()))
+        sch += (nground := Ground.NoiselessGround().translate(point = Utility.Point(5, 0) - Ground.NoiselessGround().center()))
+        sch += (pground := Ground.ProtectiveGround().translate(point = Utility.Point(6, 0) - Ground.ProtectiveGround().center()))
+        sch += (cground := Ground.ChassisGround().translate(point = Utility.Point(7, 0) - Ground.ChassisGround().center()))
+        sch += (eground := Ground.EuropeanStyleGround().translate(point = Utility.Point(8, 0) - Ground.EuropeanStyleGround().center()))
+        sch += (eground2 := Ground.EuropeanStyleGround2().translate(point = Utility.Point(9, 0) - Ground.EuropeanStyleGround2().center()))
 
         engine.draw(sch, debug = True)
         
